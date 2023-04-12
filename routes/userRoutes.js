7a07470a99b727591855061f3faea851eb5dc22a
mohit <mohitpardeshi729@gmail.com>
2023-04-12 13:10:30 +0530

    get request added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,11 @@ const { registerUser, loginUser } = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 const { postMedicalData } = require('../controllers/medicalDataController');
 
+router.get('/', () => {
+    res.status(201).json({
+        msg: 'this is the get request for backend api and it is working, kindly post',
+    });
+});
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/:userId/medicaldata', postMedicalData);
