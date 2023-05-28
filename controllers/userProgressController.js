51769dd02ee4d9f5bce1555361993c523f72c400
mohit <mohitpardeshi729@gmail.com>
2023-05-28 11:21:11 +0530

    added controller which doesn't allows to add two or morde datas for the same data

--- controllers/userProgressController.js
+++ controllers/userProgressController.js
@@ -10,18 +10,31 @@ const ProgressData = require('../models/userProgress');
  */
 
 const postProgressData = async (req, res) => {
-    try {
-        const userId = req.params.userId; // get the user id from the URL params
-        const progressRecord = new ProgressData({
-            user: userId,
-            smoked: req.body.smoked,
-        });
-        const savedRecord = await progressRecord.save();
-        res.json(savedRecord);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+  try {
+    const userId = req.params.userId; // get the user id from the URL params
+
+    // Check if progress data for the current day already exists
+    const today = new Date().toISOString().split('T')[0]; // Get the current date in YYYY-MM-DD format
+    const existingData = await ProgressData.findOne({
+      user: userId,
+      created_at: { $gte: today, $lt: new Date(today).setDate(new Date(today).getDate() + 1) },
+    });
+
+    if (existingData) {
+      return res.status(400).json({ error: 'Progress data for today already exists' });
     }
+
+    const progressRecord = new ProgressData({
+      user: userId,
+      smoked: req.body.smoked,
+    });
+
+    const savedRecord = await progressRecord.save();
+    res.json(savedRecord);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 };
 
 module.exports = { postProgressData };
